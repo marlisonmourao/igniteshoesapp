3499081f4b101faf46589f7ca405be3337cfae83
marlisonmourao <bentesmourao@gmail.com>
2023-04-11 02:30:05 -0400

    changing app flow from manager to bare workflow

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { StatusBar } from 'react-native';
+import OneSignal from "react-native-onesignal";
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -8,6 +9,8 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.setAppId("1cddcad5-61bb-4bfc-9d74-d5f152624390");
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
