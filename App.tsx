be76c110bab256d8c1dce3b86cf5e506982c9d26
marlisonmourao <bentesmourao@gmail.com>
2023-04-11 20:24:40 -0400

    config onesignal

--- App.tsx
+++ App.tsx
@@ -7,13 +7,18 @@ import { Routes } from './src/routes';
 
 import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
+import { tagUserEmailCreate } from './src/notifications/notificationsTags';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 
 OneSignal.setAppId("1cddcad5-61bb-4bfc-9d74-d5f152624390");
 
+OneSignal.setEmail("marlison@email.com")
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  tagUserEmailCreate('bentesmourao@gmail.com')
 
   return (
     <NativeBaseProvider theme={THEME}>
